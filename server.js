455ae431c85e64c29e0412f587cd3d306e1933a6
ramayaser66 <ramayaser66@gmail.com>
2021-02-21 14:08:46 +0200

    basic structure

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// importing packages...
+let express = require('express'); 
+
+
+// initialization and configuration 
+
+let app = express(); 
+require('dotenv').config();
+
+
+const PORT = process.env.PORT; 
+
+app.listen(PORT, ()=>{
+    console.log('the app is listening on port ' + PORT);
+}); 
+
